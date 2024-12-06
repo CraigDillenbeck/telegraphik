265fb47c7fd8871dc6ed354d4d2738f8b68934e6
Craig Dillenbeck <craigdillenbeck@Craigs-MacBook-Pro.local>
2024-12-06 11:16:46 -0500

    home page build, design placement

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+// function fillDivWithMorseCode() {
+//   const div = document.getElementById("morse-code-div");
+//   div.style.width = "500px";
+//   div.style.height = "500px";
+//   div.style.fontSize = "10px";
+//   div.style.fontFamily = "monospace";
+//   div.style.whiteSpace = "pre-wrap";
+
+//   let morseCode = "";
+//   for (let i = 0; i < 2500; i++) {
+//     morseCode += Math.random() < 0.5 ? "." : "-";
+//   }
+
+//   div.textContent = morseCode;
+// }
+
+// fillDivWithMorseCode();
